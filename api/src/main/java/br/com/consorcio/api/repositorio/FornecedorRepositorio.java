6a1ec198776aecd91f65e75dd2810468b22b3578
Ariel <dolzanariel@gmail.com>
2023-12-05 22:22:57 -0300

    Fornecedor Alterar

--- api/src/main/java/br/com/consorcio/api/repositorio/FornecedorRepositorio.java
+++ api/src/main/java/br/com/consorcio/api/repositorio/FornecedorRepositorio.java
@@ -1,11 +1,14 @@
 package br.com.consorcio.api.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.consorcio.api.modelo.Fornecedor;
 @Repository
 
 public interface FornecedorRepositorio extends JpaRepository<Fornecedor, Long> {
-  
+
+  @Query("SELECT fornecedor From Fornecedor fornecedor WHERE fornecedor.id = :id")
+  Fornecedor findByFornecedorId(Long id);
 }
