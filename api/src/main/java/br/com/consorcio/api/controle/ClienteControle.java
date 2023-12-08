958ddeeb02b7ea7f8b3ab37ecd1d92cae487d54e
ariel439 <dolzanariel@gmail.com>
2023-12-07 21:51:42 -0300

    Cliente Controle

--- api/src/main/java/br/com/consorcio/api/controle/ClienteControle.java
+++ api/src/main/java/br/com/consorcio/api/controle/ClienteControle.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,9 +37,21 @@ public class ClienteControle {
         return clienteServico.listarClientes();
     }
 
+    //listar por id
+    @GetMapping("/{id}")
+    public ResponseEntity<?> selecionarClienteId(@PathVariable Long id){
+        return clienteServico.selecionarClienteId(id);
+    }
+
     //alterar
     @PutMapping()
     public ResponseEntity<?> alterarCliente(@RequestBody Cliente cliente) {
         return clienteServico.alterarCliente(cliente);
+    }
+
+    //deletar
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletarCliente(@PathVariable Long id){
+        return clienteServico.deletarCliente(id);
     }
 }
