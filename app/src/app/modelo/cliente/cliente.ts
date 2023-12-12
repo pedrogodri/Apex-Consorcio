b56983b1074b8bdb06f6617faa41c067cdaf1f97
ariel439 <dolzanariel@gmail.com>
2023-12-12 19:20:14 -0300

    Cliente

---
+++ app/src/app/modelo/cliente/cliente.ts
@@ -0,0 +1,11 @@
+export class Cliente {
+    id?: number;
+    nome?: string;
+    sobrenome?: string;
+    email?: string;
+    senha?: string;
+    cpf?: string;
+    idade?: number;
+    //endereco?: Endereco;
+    telefone?: number;
+}
