0381a8769f05e1001fe2c50bd776a4fac94b85d9
PedroGodri <pedrogodri@gmail.com>
2023-12-12 21:14:16 -0300

    Feat: pastas de cliente, fornecedor e veiculo

--- app/src/app/service/exemplo/exemplo.service.ts
+++ app/src/app/service/exemplo/exemplo.service.ts
@@ -16,4 +16,28 @@ export class ExemploService {
   cadastrarExemplo(endereco: Endereco): Observable<Endereco> {
     return this.http.post<Endereco>(this.url, endereco);
   }
+
+  getExemplo(): Observable<Endereco[]> {
+    return this.http.get<Endereco[]>(this.url);
+  }
+
+  getExemploById(id: number): Observable<Endereco> {
+    return this.http.get<Endereco>(`${this.url}/${id}`);
+  }
+
+  atualizarExemplo(endereco: Endereco): Observable<any> {
+    return this.http.put<Endereco>(this.url, endereco);
+  }
+
+  deletarExemplo(endereco: Endereco): Observable<any> {
+    return this.http.delete<any>(`${this.url}/${endereco.id}`);
+  }
+
+  getExemploId(id: number) {
+    this.exemploId = id;
+  }
+
+  setExemploId() : number {
+    return this.exemploId;
+  }
 }
